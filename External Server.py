3a437833e26cbddecbd9947a144c9be8bf5e75c1
Danny Vo <dmvo@ucsd.edu>
2023-08-31 20:14:25 -0700

    Fixed server to export pkl and add tpose

--- External Server.py
+++ External Server.py
@@ -1,6 +1,8 @@
 # Simulate UDP requests to a server
+import pickle
 import socket
 import struct
+import numpy as np
 import pandas as pd
 from scipy.spatial.transform import Rotation
 
@@ -39,57 +41,78 @@ for i in range(len(addresses)):
     accels.append([0, 0, 0])
     rotations.append([0, 0, 0])
 
-while True:
-    for index, sock in enumerate(sockets):
-        try:
-            # Read any data coming from socket  
-            data, addr = sock.recvfrom(120)
-            stream = data.decode('latin1')
-            buffers[index] += data
+out_pose = []
 
-            # # Check if we have received a complete packet
-            # if len(buffers[index]) >= struct.calcsize(format_string):
-            #     # Unpack the packet using struct
-            #     packet = struct.unpack(format_string, buffers[index][:struct.calcsize(format_string)])
-            #     # Extract the data from the packet
-            #     vector = packet[:3]
-            #     quaternion = packet[3:7]
-            #     accuracy_info = packet[7]
-
-            #     # Do something with the data here
-            #     print(f"Received vector: {vector}, quaternion: {quaternion}, accuracy info: {accuracy_info}")
-
-            #     # Remove the processed data from the buffer
-            #     buffers[index] = buffers[index][struct.calcsize(format_string):]
-
-            # Process all complete packets in the buffer
-            if len(buffers[index]) >= format_string_size:
-                offset = buffers[index].find(delimiter)
+try:
+    debug_i = 0
+    while True:
+        for index, sock in enumerate(sockets):
+            try:
+                # Read any data coming from socket  
+                data, addr = sock.recvfrom(120)
+                stream = data.decode('latin1')
+                buffers[index] += data
 
                 # # Check if we have received a complete packet
-                if offset + format_string_size > len(buffers[index]):
-                    break
+                # if len(buffers[index]) >= struct.calcsize(format_string):
+                #     # Unpack the packet using struct
+                #     packet = struct.unpack(format_string, buffers[index][:struct.calcsize(format_string)])
+                #     # Extract the data from the packet
+                #     vector = packet[:3]
+                #     quaternion = packet[3:7]
+                #     accuracy_info = packet[7]
 
-                # Unpack the packet using struct
-                packet = struct.unpack_from(format_string, buffers[index][:format_string_size], offset=offset)
-                # Extract the data from the packet
-                # print(packet)
-                sensorId = packet[0]
-                accel = packet[1:4]
-                quaternion = packet[4:8]
-                accuracy_info = packet[8]
+                #     # Do something with the data here
+                #     print(f"Received vector: {vector}, quaternion: {quaternion}, accuracy info: {accuracy_info}")
 
-                # # Convert quaternion to euler angles
-                # rot = Rotation.from_quat(list(quaternion))
-                # rot_euler = rot.as_euler('xyz', degrees=True)
-                # print(accel, '\t', rot_euler.tolist())
+                #     # Remove the processed data from the buffer
+                #     buffers[index] = buffers[index][struct.calcsize(format_string):]
 
-                accels[index] = accel
-                rotations[index] = quaternion
+                # Process all complete packets in the buffer
+                if len(buffers[index]) >= format_string_size:
+                    offset = buffers[index].find(delimiter)
 
-                # Remove the processed data from the buffer
-                buffers[index] = buffers[index][format_string_size:]                
-        except socket.timeout:
-            print("Failed read")
-            pass
+                    # # Check if we have received a complete packet
+                    if offset + format_string_size > len(buffers[index]):
+                        break
+
+                    # Unpack the packet using struct
+                    packet = struct.unpack_from(format_string, buffers[index][:format_string_size], offset=offset)
+                    # Extract the data from the packet
+                    # print(packet)
+                    sensorId = packet[0]
+                    accel = packet[1:4]
+                    quaternion = packet[4:8]
+                    accuracy_info = packet[8]
+
+                    # # Convert quaternion to euler angles
+                    # rot = Rotation.from_quat(list(quaternion))
+                    # rot_euler = rot.as_euler('xyz', degrees=True)
+                    # print(accel, '\t', rot_euler.tolist())
+
+                    accels[index] = accel
+                    rotations[index] = quaternion
+
+                    # Remove the processed data from the buffer
+                    buffers[index] = buffers[index][format_string_size:]                
+            except socket.timeout:
+                print("Failed read")
+                pass
+        
+        # Add to numpy array tuple of accel and rotation
+        # Create tuple first then add to numpy array
+        all_data = []
+        for i in range(len(addresses)):
+            tu = (accels[i], rotations[i])
+            all_data.append(tu)
+        
+        out_pose.append(tuple(all_data))
+
+finally:
+    # Close the socket
+    for sock in sockets:
+        sock.close()
     
+    # Output to pkl
+    with open('pose.pkl', 'wb') as f:
+        pickle.dump(out_pose, f)
